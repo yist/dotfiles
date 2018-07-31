6555502c5f83d6c70ce1aba27bf0a4dbe6af11b5
yist <liuyiyi@gmail.com>
2018-07-31 21:25:11 +0000

    set up @ ubuntu

---
+++ setup/setup_ubuntu.sh
@@ -0,0 +1,13 @@
+set -e
+
+
+# Install latest vim
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+
+# Install latest git
+sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
